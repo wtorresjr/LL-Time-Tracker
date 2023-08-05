9f60e1d5e6dbd4c62e5c2c537d20b7ffac6df277
Will Torres <w.torresjr@gmail.com>
2023-08-05 01:50:11 -0400

    ADded route filters, for hoursworked and client

--- backend/routes/api/client-hours.js
+++ backend/routes/api/client-hours.js
@@ -1,23 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const { employee, hoursworked, client } = require("../../db/models");
+const { Op } = require("sequelize");
 
 router.get("/", async (req, res) => {
   if (req.user) {
-    console.log("Req user body", req.user);
     let { id } = req.user;
+    const { dayStart, dayEnd } = req.query;
+    const where = {};
+
+    if (dayStart || dayEnd !== undefined) {
+      if (dayStart && dayEnd) {
+        where.day_worked = { [Op.between]: [dayStart, dayEnd] };
+      } else {
+        res
+          .status(401)
+          .json({ error: "You must choose both a start and end date" });
+      }
+    }
 
     let clientHours = await employee.findByPk(id, {
       include: {
         model: hoursworked,
         attributes: ["day_worked", "start_time", "end_time", "total_hours"],
+        include: { model: client, attributes: ["client_initials"] },
+        where,
       },
+      attributes: ["firstName", "lastName"],
     });
-    // let clientHours = await hoursworked.findAll({
-    //   where: {
-    //     employeeId: id,
-    //   },
-    // });
     res.json(clientHours);
   } else {
     res.status(401).json({ error: "Unauthorized - Login to continue" });
