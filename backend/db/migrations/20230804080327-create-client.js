72ddd14cfe673598532c7c82c0bf70e844ec4a58
Will Torres <w.torresjr@gmail.com>
2023-10-08 20:12:36 -0400

    Update to deploy 3

--- backend/db/migrations/20230804080327-create-client.js
+++ backend/db/migrations/20230804080327-create-client.js
@@ -45,6 +45,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("clients");
+    await queryInterface.dropTable("Clients");
   },
 };
--- backend/db/migrations/20230804080507-create-hoursworked.js
+++ backend/db/migrations/20230804080507-create-hoursworked.js
@@ -49,6 +49,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("hoursworkeds");
+    await queryInterface.dropTable("Hoursworkeds");
   },
 };
