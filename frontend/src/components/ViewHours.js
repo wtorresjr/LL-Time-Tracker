b6d4920fe0e916a19da52a3aa0149bf45afbfdfb
Will Torres <w.torresjr@gmail.com>
2023-10-12 01:22:03 -0400

    Test Fix

--- frontend/src/components/ViewHours.js
+++ frontend/src/components/ViewHours.js
@@ -11,8 +11,12 @@ const ViewHours = () => {
   const userHrs = useSelector(
     (state) => state?.hoursReducer?.userHours?.clients
   );
-  const allPay = useSelector((state) => state?.hoursReducer?.userHours);
 
+  const allPay = useSelector(
+    (state) => state?.hoursReducer?.userHours?.All_Client_Pay
+  );
+
+  
   useEffect(() => {
     const fetchData = async () => {
       await dispatch(fetchHours(sessionUser?.id));
@@ -20,10 +24,11 @@ const ViewHours = () => {
     };
 
     fetchData();
-  }, [dispatch, sessionUser]);
+  }, [dispatch, sessionUser, allPay]);
+
 
   useEffect(() => {
-    console.log("allPay.All_Client_Pay:", allPay?.All_Client_Pay);
+    console.log("All_Client_Pay:", allPay);
   }, [allPay]);
 
   return (
@@ -32,17 +37,17 @@ const ViewHours = () => {
         <h1>Hours</h1>
       </div>
       <form>
-        {(allPay && +allPay?.All_Client_Pay != 0 && (
+        {(+allPay != 0 && (
           <Alert variant="success" id="allClientPayDiv">
             <i className="fa-solid fa-sack-dollar fa-2xl" id="iconPadding"></i>
             <div style={{ textAlign: "center" }}>
-              Current Pay (All Clients): ${allPay?.All_Client_Pay}
+              Current Pay (All Clients): ${allPay}
             </div>
             <i className="fa-solid fa-sack-dollar fa-2xl" id="iconPadding"></i>
           </Alert>
         )) ||
           !allPay ||
-          (allPay?.All_Client_Pay == 0 && (
+          (+allPay == 0 && (
             <h3 style={{ textAlign: "center" }}>
               You currently have no hours logged.
             </h3>
