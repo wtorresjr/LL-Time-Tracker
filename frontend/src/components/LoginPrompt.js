825723639602e9d1c448ce11bb7fd2f2b56a704c
Will Torres <w.torresjr@gmail.com>
2023-10-12 21:20:10 -0400

    Fix attempt 13

--- frontend/src/components/LoginPrompt.js
+++ frontend/src/components/LoginPrompt.js
@@ -61,8 +61,10 @@ function LoginPrompt() {
               placeholder="Password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
+              autoComplete="current-password"
             />
           </FloatingLabel>
+
           {errors &&
             (errors.errors || errors.password || errors.credential) && (
               <h5
