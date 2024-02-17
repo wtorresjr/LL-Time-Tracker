aa3b17de7338a6654cda65ed7775beae92dc52e1
Will Torres <w.torresjr@gmail.com>
2024-02-17 18:39:03 -0500

    added nested client accordions to admin view

--- frontend/src/components/Accordions/AdminViewAccordion.js
+++ frontend/src/components/Accordions/AdminViewAccordion.js
@@ -28,23 +28,40 @@ function AdminViewAccordion({ employeeHours }) {
                   </div>
                 </div>
               </Accordion.Header>
-              <Accordion.Body>
-                <div className="hourlynPayDiv"></div>
-                <Table bordered>
-                  <thead>
-                    <tr style={{ display: "flex", width: "100%" }}>
-                      <th style={{ width: "25%" }}>Date:</th>
-                      <th style={{ width: "25%" }}>Hours:</th>
-                      <th style={{ width: "25%", textAlign: "center" }}>
-                        Amount Earned:
-                      </th>
-                      <th style={{ width: "25%", textAlign: "center" }}>
-                        Click if paid:
-                      </th>
-                    </tr>
-                  </thead>
-                </Table>
-              </Accordion.Body>
+
+              {employee?.clients &&
+                employee?.clients?.map((client) => {
+                  return (
+                    <Accordion.Body key={client?.client_initials}>
+                      <Accordion.Header>
+                        <div className="accHeader">
+                          <div>{client.client_initials} - Hours</div>
+                          <div>
+                            Total Owed Employee:{" "}
+                            <Badge bg="success" style={{ fontSize: "16px" }}>
+                              ${employee?.Total_Employee_Pay}
+                            </Badge>
+                          </div>
+                        </div>
+                      </Accordion.Header>
+                      <div className="hourlynPayDiv"></div>
+                      <Table bordered>
+                        <thead>
+                          <tr style={{ display: "flex", width: "100%" }}>
+                            <th style={{ width: "25%" }}>Date:</th>
+                            <th style={{ width: "25%" }}>Hours:</th>
+                            <th style={{ width: "25%", textAlign: "center" }}>
+                              Amount Earned:
+                            </th>
+                            <th style={{ width: "25%", textAlign: "center" }}>
+                              Click if paid:
+                            </th>
+                          </tr>
+                        </thead>
+                      </Table>
+                    </Accordion.Body>
+                  );
+                })}
             </Accordion.Item>
           );
         })}
